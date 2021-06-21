a17c9ba67c5e86706a93ea196af339407bc3d65a
KASHIF-IQBAL-KHAN <kashifiqbalkhan30@gmail.com>
2021-06-21 23:32:41 +0530

    Easy

---
+++ Top Interview Questions/LinkedList/Remove Nth Node From End of List.java
@@ -0,0 +1,68 @@
+/*
+Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function would be added by LeetCode's Online Judge.
+
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+Example 1:
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+Example 2:
+
+Input: head = [1], n = 1
+Output: []
+Example 3:
+
+Input: head = [1,2], n = 1
+Output: [1]
+
+Constraints:
+
+The number of nodes in the list is sz.
+1 <= sz <= 30
+0 <= Node.val <= 100
+1 <= n <= sz
+ 
+Follow up: Could you do this in one pass?
+*/
+
+// Kashif Iqbal
+// 21-June-2021
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode dummyListHead = new ListNode();
+        dummyListHead.next = head;
+        ListNode slow = dummyListHead;
+        ListNode fast = dummyListHead;
+        int count = 0;
+
+        while (count <= n && fast != null) {
+            count = count + 1;
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+        return dummyListHead.next;
+    }
+}
